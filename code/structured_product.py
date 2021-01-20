427460c99f26c65d46fbf00979a67ed07e8285f8
Shukai Wang <christopher729sc@gmail.com>
2021-01-19 22:13:52 -0500

    Initial Commit

---
+++ code/structured_product.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 19 21:48:49 2021
+
+@author: Shukai Wang
+"""
+
+import argparse
+import numpy as np
+import pandas as pd
+import utils
+
+
+
+class YieldCurve:
+    
+    def __init__(self):
+        
+        pass
